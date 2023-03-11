e73d165a3c9ee3641065486bb9b5e6f6b5d37d8a
Amelss <ameleyka@gmail.com>
2023-03-11 23:02:05 +0000

    desktop navbar created

--- from-scratch/components/Navbar.js
+++ from-scratch/components/Navbar.js
@@ -10,15 +10,15 @@ export default function Navbar() {
           <div className="logo">
             <Image src={"/logo.png"} width={100} height={100} />
           </div>
-          <div className="nav-links">
-            <Link href={'/'}>Home</Link>
-            <Link href={'/'}>About Us</Link>
-            <Link href={'/'}>Sessions</Link>
-            <Link href={'/'}>Event</Link>
+            <div className="nav-links">
+              <Link href={'/'}>Home</Link>
+              <Link href={'/'}>About Us</Link>
+              <Link href={'/'}>Sessions</Link>
+              <Link href={'/'}>Event</Link>
+            </div>
           </div>
-              </div>
-              <div className='donate-btn'>
-                  <Link href={'/'}><button>Donate</button></Link>
+              <div className='donate-btn-container'>
+                  <Link href={'/'}><button className='donate-btn'>Donate</button></Link>
               </div>
       </nav>
     </div>
